ab0d90c2aacec10a0b72d63aeed4336507820996
Igor.Santos <igmascaras58@gmail.com>
2022-01-26 22:37:36 -0300

    Terceiro dia

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e5e666ca717a131"; }
+/******/ 		__webpack_require__.h = function() { return "9ca927b3e0a87db6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
